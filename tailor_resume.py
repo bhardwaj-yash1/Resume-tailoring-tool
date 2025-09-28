6ee22864b3c185dc2923375767f42ad599b38785
Yash bhardwaj <b523075@iiit-bh.ac.in>
2025-09-28 16:40:28 +0530

    updated

--- tailor_resume.py
+++ tailor_resume.py
@@ -2,3 +2,15 @@ import os
 from utils.llm_utils import resume_tailoring_tool
 from utils.pdf_utils import read_pdf
 
+pdf_path=os.getenv("pdf_path")
+latex_path=os.getenv("latex_path")
+api=os.getenv("api_key")
+
+resume_text=read_pdf(pdf_path=pdf_path)
+
+with open(latex_path,"r",encoding="utf-8") as f:
+    latex_code= f.read()
+
+jd=str(input("enter job desc:"))
+
+resume_tailoring_tool(resume_text=resume_text,jd_text=jd,latex_code=latex_code,api_key=api)
--- utils/pdf_utils.py
+++ utils/pdf_utils.py
@@ -5,3 +5,8 @@ def read_pdf(pdf_path:str)->str:
     page=doc[0]
     content=page.get_text()
     return content
+
+
+def save_latex_code(latex:str,save_path:str):
+    with open(save_path, "w", encoding="utf-8") as f:
+        f.write(latex)
